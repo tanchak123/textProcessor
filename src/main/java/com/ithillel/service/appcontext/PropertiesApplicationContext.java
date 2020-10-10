a6bfe4f94323ef397b57881a091dbd4a95d36272
Andrey Tanchak <61573898+tanchak123@users.noreply.github.com>
2020-10-10 09:49:04 +0300

    Update PropertiesApplicationContext.java

--- src/main/java/com/ithillel/service/appcontext/PropertiesApplicationContext.java
+++ src/main/java/com/ithillel/service/appcontext/PropertiesApplicationContext.java
@@ -8,23 +8,18 @@ import java.util.Properties;
 
 public class PropertiesApplicationContext implements ApplicationContext {
 
-    private static final String TEXT_PROCESSOR = "textProcessor";
-    private Map<String, Object> beans = new HashMap<>();
+      private static Map<String, Object> beans = new HashMap<>();
 
     public PropertiesApplicationContext() {
         Properties applicationProperties = new Properties();
         try {
             applicationProperties.load(getClass().getClassLoader()
                     .getResourceAsStream("application.properties"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         for (int i = 0; i < 2; i++) {
             String name = applicationProperties.getProperty("beans[" + i + "].name");
             System.out.println(name);
             String type = applicationProperties.getProperty("beans[" + i + "].type");
-            try {
-                if (name.equals("textStorage")) {
+            if (name.equals("textStorage")) {
                     Object bean = getBean(applicationProperties.getProperty("beans[1].args"));
                     System.out.println(bean);
                     beans.put(name, Class.forName(type).getDeclaredConstructor(Storage.class)
@@ -32,9 +27,9 @@ public class PropertiesApplicationContext implements ApplicationContext {
                 } else {
                     beans.put(name, Class.forName(type).getDeclaredConstructor().newInstance());
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
