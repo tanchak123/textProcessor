33f3858be18bc62364181872b59a45b48ba99c7d
Andrey Tanchak <tanchak12@gmail.com>
2020-11-05 04:54:57 +0200

    added my own Thread-Demon monitor

--- src/main/java/com/ithillel/service/textprocessor/FileTextProcessor.java
+++ src/main/java/com/ithillel/service/textprocessor/FileTextProcessor.java
@@ -7,7 +7,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Calendar;
 import java.util.Objects;
 
 public class FileTextProcessor implements TextProcessor {
